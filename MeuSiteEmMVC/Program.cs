d4248fd74c9afdc962abc4656c949bfe6a9d6fab
GustavoAe <gustavoaedias05@hotmail.com>
2023-03-12 10:51:10 -0300

    Criar Contato e Listagem de contato

--- MeuSiteEmMVC/Program.cs
+++ MeuSiteEmMVC/Program.cs
@@ -1,7 +1,19 @@
+using ControleContatos.Data;
+using ControleContatos.Repositorio;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Conexão com o postgreSQL:
+builder.Services
+    .AddDbContext<BancoContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ServerConnection")));
+
+//Injecção de Dependencia : 
+builder.Services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
 
 //Habilitando a compilação em tempo de execução 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
@@ -25,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Contato}/{action=Index}/{id?}");
 
 app.Run();
